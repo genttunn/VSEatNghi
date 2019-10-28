38406b0b6973843975e94b4e2878f38b24ab4527
mia <mannghi.tranvn@gmail.com>
2019-10-28 09:20:02 +0100

    second commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSEatNghi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
